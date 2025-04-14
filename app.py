2df50b0b4a3a4114641a1297cf40a0beb89ea00f
P-k-s09 <demoemailpks@gmail.com>
2025-04-15 02:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def index():
             result = calculate_similarity(text1, text2)
     return render_template("index.html", result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
